b27067ef1a17c001dd2b6879de7bfd3922358f66
Dimitris Mantzouranis <d3xter93@gmail.com>
2025-05-16 00:17:23 +0300

    platforms: sn32: WFI idle is set by default now

--- platforms/chibios/boards/SN_SN32F240C/board/board.mk
+++ platforms/chibios/boards/SN_SN32F240C/board/board.mk
@@ -6,8 +6,6 @@ BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240C
 
 # Optimize for speed
 OPT = 2
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 # Shave some extra bytes
 OPT_DEFS += -DCRT1_AREAS_NUMBER=1
 
