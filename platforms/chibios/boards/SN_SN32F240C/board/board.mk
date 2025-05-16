7a562559b43068f154d8aaf14426adcce7294f52
Dimitris Mantzouranis <d3xter93@gmail.com>
2025-05-16 14:15:08 +0300

    platfroms: sn32: update configs

--- platforms/chibios/boards/SN_SN32F240C/board/board.mk
+++ platforms/chibios/boards/SN_SN32F240C/board/board.mk
@@ -6,8 +6,6 @@ BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240C
 
 # Optimize for speed
 OPT = 2
-# Shave some extra bytes
-OPT_DEFS += -DCRT1_AREAS_NUMBER=1
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
--- platforms/chibios/boards/SN_SN32F260/board/board.mk
+++ platforms/chibios/boards/SN_SN32F260/board/board.mk
@@ -8,7 +8,7 @@ BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F260
 # OPT = s /set by default
 
 # Shave some extra bytes
-OPT_DEFS += -DCRT1_AREAS_NUMBER=1
+OPT_DEFS += -DCRT0_AREAS_NUMBER=1
 
 # Some options to reduce RAM usage
 USE_LINK_GC = yes
