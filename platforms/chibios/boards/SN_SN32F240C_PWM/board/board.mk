edf20188e88a610d794b26cfa420695638fcb524
Dimitris Mantzouranis <d3xter93@gmail.com>
2026-01-16 16:01:19 +0200

    boards: add sn32f240c_shared_matrix

---
+++ platforms/chibios/boards/SN_SN32F240C_PWM/board/board.mk
@@ -0,0 +1,17 @@
+# List of all the board related files.
+BOARDSRC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240C/board.c
+
+# Extra files
+BOARDSRC += $(BOARD_PATH)/board/extra.c
+
+# Required include directories
+BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240C
+
+# Optimize for speed
+OPT = 2
+# Shave some extra bytes
+OPT_DEFS += -DCRT0_AREAS_NUMBER=1
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
